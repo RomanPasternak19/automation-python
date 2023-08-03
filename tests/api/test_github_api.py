741d52932851709a6da782a4cc397c9487aaa3f7
RomanPasternak <roman.pasternak19@gmail.com>
2023-08-03 12:57:46 +0300

    added api tests

---
+++ tests/api/test_github_api.py
@@ -0,0 +1,34 @@
+import pytest
+from modules.api.clients.github import GitHub
+
+
+@pytest.mark.api
+def test_user_exists(github_api):
+    user = github_api.get_user('defunkt')
+
+    assert user['login'] == 'defunkt'
+
+@pytest.mark.api
+def test_user_not_exist(github_api):
+    r = github_api.get_user('romanpasternak')
+
+    assert r['message'] == 'Not Found'
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+
+    assert r['total_count'] == 13
+    assert 'become-qa-auto' in r['items'][0]['name']
+
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):
+    r = github_api.search_repo('roman_repo_non_exist')
+
+    assert r['total count'] == 0
+
+@pytest.mark.api
+def test_repo_with_single_char_be_found(github_api):
+    r = github_api.search_repo('s')
+
+    assert r['total_count'] != 0
