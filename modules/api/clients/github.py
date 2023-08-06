2eda404f9aee4105ddcf309e7dd8eea24f4436d5
Roman <roman.pasternak19@gmail.com>
2023-08-07 01:34:03 +0300

    added api tests for commits and emojis

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -4,7 +4,7 @@ import requests
 class GitHub:
 
     def get_user(self, username):
-        r = requests.get(f'https://api.github.com/users/{username}')
+        r = requests.get(f"https://api.github.com/users/{username}")
         body = r.json()
 
         return body
@@ -14,6 +14,18 @@ class GitHub:
             "https://api.github.com/search/repositories",
             params={"q": name}
         )
+        body = r.json()
+
+        return body
+    
+    def get_commits(self, owner, repo):
+        r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+        body = r.json()
+
+        return body
+    
+    def get_emojis(self):
+        r = requests.get(f"https://api.github.com/emojis")
         body = r.json()
 
         return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -10,25 +10,49 @@ def test_user_exists(github_api):
 
 @pytest.mark.api
 def test_user_not_exist(github_api):
-    r = github_api.get_user('romanpasternak')
-
+    r = github_api.get_user('romanpasternakwrong')
+    
     assert r['message'] == 'Not Found'
 
 @pytest.mark.api
 def test_repo_can_be_found(github_api):
     r = github_api.search_repo('become-qa-auto')
-
-    assert r['total_count'] == 13
+    
+    assert r['total_count'] == 43
     assert 'become-qa-auto' in r['items'][0]['name']
 
 @pytest.mark.api
 def test_repo_cannot_be_found(github_api):
     r = github_api.search_repo('roman_repo_non_exist')
-
-    assert r['total count'] == 0
+    
+    assert r['total_count'] == 0
 
 @pytest.mark.api
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo('s')
 
     assert r['total_count'] != 0
+
+@pytest.mark.api
+def test_check_number_commits(github_api):
+    r = github_api.get_commits('RomanPasternak19', 'automation-cypress-test-allo')
+    
+    assert len(r) == 4
+
+@pytest.mark.api
+def test_check_commit_messege(github_api):
+    r = github_api.get_commits('RomanPasternak19', 'automation-cypress-test-allo')
+    
+    assert r[0]['commit']['message'] == "Update .gitignore"
+
+@pytest.mark.api
+def test_check_commit_author_name(github_api):
+    r = github_api.get_commits('RomanPasternak19', 'automation-cypress-test-allo')
+    
+    assert r[0]['commit']['author']['name'] == "Roman Pasternak"
+
+@pytest.mark.api
+def test_emojis_count(github_api):
+    r = github_api.get_emojis()
+    
+    assert len(r) > 0
